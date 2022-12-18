efa12ad09f786500b7aceeedb4be75dc64ac76c1
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-12-19 02:56:22 +0530

    Update login.c

--- login.c
+++ login.c
@@ -1,37 +1,80 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+
+void red () {
+  printf("\033[1;31m");
+}
+void yellow(){
+   printf("\033[0;33m");
+}
+void reset() {
+  printf("\033[0m");
+}
+void blue(){
+   printf("\033[0;34m");
+}
+void purple(){
+   printf("\033[0;35m");
+}
+void green(){
+   printf("\033[0;32m");
+}
+
+
+
 int main()
 {
   
-   char username[10] , password[10] ;
-   char un_default[]="abcd";
-   char pass_default[]="12345";
+   char username[15] , password[10] ;
+   char un_default[]="movie_maniac";
+   char pass_default[]="@b123";
    int x,y,n=1;
+   
+
+  blue();
+  printf("-------------------------------------------------------------------------------\n");
+  printf("||###########################################################################||\n");
+  printf("                                                                              \n");
+  yellow();
+  printf("                        WELCOME TO MOVIE MANIAC\n"); 
+  blue();
+  printf("                                                                              \n");
+  printf("||###########################################################################||\n");
+  printf("-------------------------------------------------------------------------------\n"); 
+  reset();
+
   do
   {
-   printf("\nEnter the username: ");
-   scanf("%s",&username);
+   blue();
+   printf("\nEnter the username: \n");
+   scanf("%s",&username); 
 
-   printf("\nEnter the password: ");
+   printf("\nEnter the password: \n");
    scanf("%s",&password);
    x=strcmp(username, un_default);
    y=strcmp(password, pass_default);
 
    if(x == 0){
 		if(y == 0){
+         green();
 			printf("\nUser successfully logged in...\n");
+         reset();
          break;
 		}else{
+         yellow();
 			printf("\nPassword entered is invalid");
 		}
 	}else{
+      yellow();
 		printf("\nUsername entered is invalid");
 	}
    n++;
    
    if(n>5){
-      printf("\naccess denied\n");
+      red();
+      printf("\n***access denied***\n");
+      reset();
       exit(0);
    }
 
