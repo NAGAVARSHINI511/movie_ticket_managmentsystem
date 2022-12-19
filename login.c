b57eb4bc106a1a63c8198dfd1c46d17c2548baa3
NAGAVARSHINI511 <nagavarshini511@gmail.com>
2022-12-19 11:35:01 +0530

    trial

--- login.c
+++ login.c
@@ -1,25 +1,34 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include<windows.h>
+
+
 void red () {
   printf("\033[1;31m");
 }
+
 void yellow(){
    printf("\033[0;33m");
 }
+
 void reset() {
   printf("\033[0m");
 }
+
 void blue(){
    printf("\033[0;34m");
 }
+
+
 void purple(){
    printf("\033[0;35m");
 }
+
 void green(){
    printf("\033[0;32m");
 }
+
+
 
 
 
